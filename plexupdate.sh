080f7a8ef14e43f26069c1a99093de04bbe543af
edwinkloostermanhtx <70527203+edwinkloostermanhtx@users.noreply.github.com>
2021-11-12 11:36:58 +0100

    Revert "Update plexupdate.sh"
    This reverts commit 0daca557323f8a867b8c29dca30de9307c8f1488.

--- plexupdate.sh
+++ plexupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script to automagically update Plex Media Server on Synology NAS
+# Script to automatically update Plex Media Server on Synology NAS (DSM 7)
 #
 # Must be run as root.
 #
@@ -12,16 +12,18 @@ mkdir -p /tmp/plex/ > /dev/null 2>&1
 token=$(cat /volume1/Plex/Library/Application\ Support/Plex\ Media\ Server/Preferences.xml | grep -oP 'PlexOnlineToken="\K[^"]+')
 url=$(echo "https://plex.tv/api/downloads/5.json?channel=plexpass&X-Plex-Token=$token")
 jq=$(curl -s ${url})
-newversion=$(echo $jq | jq -r .nas.Synology.version)
+newversion=$(echo $jq | jq -r '.nas."Synology (DSM 7)".version')
+newversion=$(echo $newversion | grep -oP '^.+?(?=\-)')
 echo New Ver: $newversion
-curversion=$(synopkg version "Plex Media Server")
+curversion=$(synopkg version "PlexMediaServer")
+curversion=$(echo $curversion | grep -oP '^.+?(?=\-)')
 echo Cur Ver: $curversion
 if [ "$newversion" != "$curversion" ]
 then
 echo New Vers Available
 /usr/syno/bin/synonotify PKGHasUpgrade '{"[%HOSTNAME%]": $(hostname), "[%OSNAME%]": "Synology", "[%PKG_HAS_UPDATE%]": "Plex", "[%COMPANY_NAME%]": "Synology"}'
 CPU=$(uname -m)
-url=$(echo "${jq}" | jq -r '.nas.Synology.releases[] | select(.build=="linux-'"${CPU}"'") | .url')
+url=$(echo "${jq}" | jq -r '.nas."Synology (DSM 7)".releases[] | select(.build=="linux-'"${CPU}"'") | .url')
 /bin/wget $url -P /tmp/plex/
 /usr/syno/bin/synopkg install /tmp/plex/*.spk
 sleep 30
